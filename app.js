2f920fdcb746a46d6b95a3c9bdbecfd7b60f4341
Nico Neiman <niconeiman@gmail.com>
2023-10-08 13:38:45 -0500

    add counter to WebSocket

--- app.js
+++ app.js
@@ -10,3 +10,32 @@ app.get('/', function (_, res) {
 server.on('request', app);
 
 server.listen(3000, () => console.log('Server started on port 3000'));
+
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+	const numberOfClients = wss.clients.size;
+	console.log(`Clients connected: ${numberOfClients}`);
+
+	wss.broadcast(stringify({ visitors: numberOfClients }));
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send(stringify({ message: 'Welcome to my server' }));
+	}
+
+	ws.on('close', function close() {
+		wss.broadcast(stringify({ visitors: numberOfClients }));
+		console.log('A client has disconnected');
+	});
+});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
+};
+
+function stringify(data) {
+	return JSON.stringify(data);
+}
