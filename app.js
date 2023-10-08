1605aaf770701b26b9a2309f50b2793adb0acff7
Nico Neiman <niconeiman@gmail.com>
2023-10-08 16:15:17 -0500

    db

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const server = require('http').createServer();
-const { stringify } = require('./utils');
-
 const app = express();
+const { stringify } = require('./utils');
+const sqlite = require('sqlite3');
 
 app.get('/', function (_, res) {
 	res.sendFile('index.html', { root: __dirname });
@@ -11,6 +11,16 @@ app.get('/', function (_, res) {
 server.on('request', app);
 
 server.listen(3000, () => console.log('Server started on port 3000'));
+
+process.on('SIGINT', () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+
+	server.close(() => {
+		shutdownDB();
+	});
+});
 
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server });
@@ -25,7 +35,13 @@ wss.on('connection', function connection(ws) {
 		ws.send(stringify({ message: 'Welcome to my server' }));
 	}
 
+	db.run(`
+	INSERT INTO visitors (counts, time)
+	VALUES (${numberOfClients}, datetime('now'))
+	`);
+
 	ws.on('close', function close() {
+		wss.broadcast(stringify({ visitors: numberOfClients }));
 		console.log('A client has disconnected');
 	});
 });
@@ -35,3 +51,23 @@ wss.broadcast = function broadcast(data) {
 		client.send(data);
 	});
 };
+
+const db = new sqlite.Database(':memory:');
+db.serialize(() => {
+	db.run(`
+	CREATE TABLE visitors (
+		counts INTEGER,
+		time TEXT
+	)`);
+});
+
+function shutdownDB() {
+	db.serialize(function getCountsAndClose() {
+		db.each('SELECT * FROM visitors', (err, row) => {
+			console.log(row);
+		});
+
+		console.log('Shutting down db');
+		db.close();
+	});
+}
